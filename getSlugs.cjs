55312314e74c3bf43e598896a64982df77468628
nefftosolution <nefftosolution@gmail.com>
2026-09-09 17:40:09 +0500

    some files deleted

--- getSlugs.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('src/data/serviceData.js', 'utf8');
-
-const regex = /slug:\s*"([^"]+)"/g;
-let match;
-while ((match = regex.exec(content)) !== null) {
-  console.log(match[1]);
-}
